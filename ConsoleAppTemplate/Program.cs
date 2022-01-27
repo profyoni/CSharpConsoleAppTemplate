22fe45a5fecb4674c4f79a1e4d4f2769ac732ef2
Jonathan Robinson <Jonathan.Robinson@turkprime.com>
2022-01-27 10:39:40 -0500

    Add project files.

---
+++ ConsoleAppTemplate/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+
+namespace ConsoleAppTemplate
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            WriteLine("Hello, World!");
+        }
+    }
+}
+
+
